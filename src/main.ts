7f5013ca0a015cb5629c1f6c863a13cd0212e7dd
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-09-10 23:47:51 +0800

    revert

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// If youre using Cesium version >= 1.42.0 add this line
-Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTY1NDYzYS01YzgxLT'
-  + 'Q2MGUtODBiYy0zODRmY2MwOGY4MDIiLCJpZCI6MjA1LCJpYXQiOjE1MDQ3MjQ1Njh9.rKgXUKAfFiiSAm_b9T8bpsDVdj0YyZeqGxNpzLlhxpk';
-
 if (environment.production) {
   enableProdMode();
-  Cesium.buildModuleUrl.setBaseUrl('/angular-cesium-sample/assets/cesium/');
 }
+
+// If youre using Cesium version >= 1.42.0 add this line
+Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));

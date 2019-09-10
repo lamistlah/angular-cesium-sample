2d15eb34f63357993d5416d6dd2c07a8509b85ca
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-09-10 23:06:20 +0800

    remove cesium_base_url

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-// eslint-disable-next-line dot-notation
-window['CESIUM_BASE_URL'] = '/assets/cesium/';
-Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/'); // If youre using Cesium version >= 1.42.0 add this line
+
+Cesium.buildModuleUrl.setBaseUrl('./assets/cesium/'); // If youre using Cesium version >= 1.42.0 add this line
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));

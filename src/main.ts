44bdd44d06de8e9d52bb6583ed858363caa675be
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-09-10 23:37:36 +0800

    added a defaultaccesstoken

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-Cesium.buildModuleUrl.setBaseUrl('./assets/cesium/'); // If youre using Cesium version >= 1.42.0 add this line
+// If youre using Cesium version >= 1.42.0 add this line
+Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTY1NDYzYS01YzgxLT'
+  + 'Q2MGUtODBiYy0zODRmY2MwOGY4MDIiLCJpZCI6MjA1LCJpYXQiOjE1MDQ3MjQ1Njh9.rKgXUKAfFiiSAm_b9T8bpsDVdj0YyZeqGxNpzLlhxpk';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));

4292a4c88e8996deb23db8ae1b03588245cfc9b4
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-09-10 22:51:36 +0800

    updated tsconfig

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+window.CESIUM_BASE_URL = '/assets/cesium/';
 Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/'); // If youre using Cesium version >= 1.42.0 add this line
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));

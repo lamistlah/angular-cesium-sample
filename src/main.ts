95ec934c37cea25e9ff6208093d2af63016e8a59
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-09-10 23:51:59 +0800

    added a temp fix for gh-pages

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
 // If youre using Cesium version >= 1.42.0 add this line
 Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
+
+if (environment.production) {
+  enableProdMode();
+  Cesium.buildModuleUrl.setBaseUrl('/angular-cesium-sample/assets/cesium/');
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));

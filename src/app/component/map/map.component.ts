5abb746f30e19389c73483e07518021a7e431fa2
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-09-28 16:20:39 +0800

    remove tslint added eslint-plugin-prettier updated eslint config added cesium WebMercatorProjection

--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -4,29 +4,29 @@ import {
   MapLayerProviderOptions,
   MapsManagerService,
   SceneMode,
-  ViewerConfiguration,
-} from 'angular-cesium';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+  ViewerConfiguration
+} from "angular-cesium";
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
+  selector: "app-map",
+  templateUrl: "./map.component.html",
   providers: [ViewerConfiguration],
-  styleUrls: ['./map.component.scss'],
+  styleUrls: ["./map.component.scss"]
 })
 export class MapComponent implements AfterViewInit {
   sceneMode = SceneMode.SCENE2D;
 
   Cesium = Cesium;
 
-  @ViewChild('mainMap', { static: false }) mainMap: MapComponent;
+  @ViewChild("mainMap", { static: false }) mainMap: MapComponent;
 
   MapLayerProviderOptions = MapLayerProviderOptions;
 
   constructor(
     private viewerConf: ViewerConfiguration,
     private mapsManagerService: MapsManagerService,
-    private draggableToMapService: DraggableToMapService,
+    private draggableToMapService: DraggableToMapService
   ) {
     this.viewerConf.viewerOptions = {
       selectionIndicator: false,
@@ -41,21 +41,22 @@ export class MapComponent implements AfterViewInit {
       navigationHelpButton: false,
       navigationInstructionsInitiallyVisible: false,
       mapMode2D: this.Cesium.MapMode2D.INFINITE_SCROLL,
+      mapProjection: new Cesium.WebMercatorProjection()
     };
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     this.viewerConf.viewerModifier = (viewer: any): void => {
       viewer.screenSpaceEventHandler.removeInputAction(
-        this.Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK,
+        this.Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK
       );
     };
   }
 
   ngAfterViewInit(): void {
     // example for getting the viewer by Id outside of the ac-map hierarchy
-    this.mapsManagerService.getMap('main-map');
+    this.mapsManagerService.getMap("main-map");
     this.draggableToMapService.dragUpdates().subscribe((e) => console.log(e));
   }
 }
 
-export default 'MapComponent';
+export default "MapComponent";

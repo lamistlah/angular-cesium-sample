51a9afa43b661aeed479429d7f8ed3c9156ba7f2
bobbysy <25177772+bobbysy@users.noreply.github.com>
2019-10-07 20:42:27 +0800

    Added sonarjs

--- src/app/component/map-layer/map-layer.data.ts
+++ src/app/component/map-layer/map-layer.data.ts
@@ -30,7 +30,7 @@ export const mapLayerDataList: MapLayerData[] = [
     id: "UrlTemplateImagery",
     provider: MapLayerProviderOptions.UrlTemplateImagery,
     options: {
-      url: "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+      url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
       credit:
         "Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</br>" +
         `[Contains information from Weather Forecast accessed on ${new Date()} from <a href='https://www.nea.gov.sg/'>National Environment Agency</a> which is made available under the terms of the <a href='https://data.gov.sg/open-data-licence'>Singapore Open Data Licence version 1.0</a>]`
